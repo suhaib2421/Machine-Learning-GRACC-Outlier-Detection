82583d8306f293853d2023d617b9bdf330936410
sshaikh <sshaikh@huskers.unl.edu>
2020-08-12 14:08:12 -0500

    Send outlier picture as email

---
+++ sendMail.py
@@ -0,0 +1,27 @@
+import smtplib
+import imghdr
+import config
+from email.message import EmailMessage
+import ml
+import datetime as dt
+import time
+
+
+msg = EmailMessage()
+msg['Subject'] = "Daily Outliers"
+msg['From'] = config.EMAIL_ADDRESS
+msg['To'] = config.EMAIL_ADDRESS
+msg.set_content("Here are the outliers for today. See image attached: ")
+
+with open('outliers.png', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(config.EMAIL_ADDRESS, config.PASSWORD)
+
+    smtp.send_message(msg)
