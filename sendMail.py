da8dfc8748dc6b7f162d5a2250b423ccad4db4cb
sshaikh <sshaikh@huskers.unl.edu>
2021-05-03 22:00:40 -0500

    formatted table and put smtp and port as env variables

--- sendMail.py
+++ sendMail.py
@@ -23,6 +23,8 @@ table = tabulate(data, headers=["VO", "Site"])
 
 userName = os.getenv('SECRET_USERNAME')
 password = os.getenv('SECRET_PASSWORD')
+mailSMTP = os.getenv('mailSMTP')
+port = os.getenv('port')
 
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "Daily Outliers"
@@ -35,13 +37,17 @@ html = transform("""\
     <html>
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <style type="text/css">
+    th, td {{padding: 12px 15px; text-align: center;}}
+    tr {{border-bottom: 1px solid #A9A9A9;}}
+    th {{border-bottom: 2px solid black;}}
+    </style>
     </head>
         <body>
             <p>Here are the outliers for today.</p>
             <p>
-            <table class="table table-bordered">{}</table>
+            {}
             </p>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         </body>
     </html>
     """.format(HTMLtable))
@@ -61,7 +67,7 @@ img.add_header('Outliers', '<{}>'.format('outliers.png'))
 msg.attach(img)
 
 # Sending the email
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+with smtplib.SMTP_SSL(mailSMTP, port) as smtp:
     smtp.login(userName, password)
 
     smtp.sendmail(userName, userName, msg.as_string())
